d63aa6b9a528254af9ad35f3ae406505a8161709
AndyXu <xu9529@gmail.con>
2019-04-10 20:00:32 +0800

    spring-dace-learning init

---
+++ dace-learning/src/main/java/com/spring/learning/dacelearning/DaceLearningApplication.java
@@ -0,0 +1,13 @@
+package com.spring.learning.dacelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaceLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaceLearningApplication.class, args);
+    }
+
+}
