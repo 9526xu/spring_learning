c134a1fecbde4338a615ae8f9780ba2f3c472257
AndyXu <xu9529@gmail.com>
2019-04-16 21:56:06 +0800

    增加 SQL 异常测试

--- dace-learning/src/test/java/com/spring/learning/dacelearning/SQLExceptrionTest.java
+++ dace-learning/src/test/java/com/spring/learning/dacelearning/SQLExceptrionTest.java
@@ -1,32 +1,40 @@
 package com.spring.learning.dacelearning;
 
+import org.apache.commons.dbutils.QueryRunner;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 /**
  * @author andyxu
  * @version V1.0
  * @Date 2019/4/11 21:08
- * @since
- *
- * SQL 异常返回码测试
+ * @since SQL 异常返回码测试
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SQLExceptrionTest {
 
+    @Autowired
+    DataSource dataSource;
+
+    QueryRunner queryRunner;
 
     @Before
-    public void buildDataSource(){
-
+    public void buildDataSource() {
+        queryRunner = new QueryRunner(dataSource);
     }
 
 
-    @Test
-    public void testSqlErrorCode(){
-
+    @Test(expected = SQLException.class)
+    public void testWhenSqlError() throws SQLException {
+        String sql="select * from1 test";
+        queryRunner.execute(sql);
     }
 }
